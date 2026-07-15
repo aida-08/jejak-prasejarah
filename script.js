aff85e4a441804ec671618599d8c30e661d00fda
aida-08 <g-18547182@moe-dl.edu.my>
2026-07-15 12:15:48 +0800

    Implement result submission to Google Sheets
    Added functionality to send quiz results to Google Sheets.

--- script.js
+++ script.js
@@ -49,7 +49,25 @@ document.getElementById("paparNama").innerHTML="👤 "+nama;
 document.getElementById("paparKelas").innerHTML="🏫 "+kelas;
 
 document.getElementById("paparMarkah").innerHTML=markah+"/15";
+// Hantar rekod ke Google Sheet
 
+let data = {
+  nama: nama,
+  kelas: kelas,
+  markah: markah+"/15"
+};
+
+fetch("https://script.google.com/macros/s/AKfycbyd7JcGUHn9pgGkebE62EojCoBngq82wvii0afBZ15dEES3nDdPW-_tkCLrQiurGVyY/exec", {
+  method: "POST",
+  body: JSON.stringify(data)
+})
+.then(response => response.text())
+.then(result => {
+  console.log("Rekod berjaya dihantar");
+})
+.catch(error => {
+  console.log("Ralat:", error);
+});
 document.getElementById("paparGelaran").innerHTML=gelaran;
 
 document.getElementById("keputusan").style.display="block";
